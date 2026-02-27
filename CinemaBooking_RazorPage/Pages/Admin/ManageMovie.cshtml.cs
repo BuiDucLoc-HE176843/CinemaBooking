5d47e06c937da59f5802289fc9ca709c2f6c8f2c
BuiDucLoc-HE176843 <buiducloc1509@gmail.com>
2026-02-27 22:14:54 +0700

    RazorPage - ManageMovie

---
+++ CinemaBooking_RazorPage/Pages/Admin/ManageMovie.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CinemaBooking_RazorPage.Pages.Admin
+{
+    public class ManageMovieModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
